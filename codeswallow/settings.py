112f905bb05629b453500c0ca837ee561633278e
Isai Ramirez <86127102+CodeSwallow@users.noreply.github.com>
2023-10-11 05:43:53 -0600

    feat: migrated user model

--- codeswallow/settings.py
+++ codeswallow/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 0))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 # Application definition
 
